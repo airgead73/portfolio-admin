0d1c8d5808280f675dc2339f6676b3c2deba2be7
Dan Moneypenny <catullus1973@gmail.com>
2022-11-24 12:08:18 -0800

    update

--- app/middleware/handleAuth.js
+++ app/middleware/handleAuth.js
@@ -1,9 +1,13 @@
 const checkAuthClient = function(req, res, next) {
   const { isAuthenticated } = res.locals;
 
+  console.log('check auth client')
+
   if(isAuthenticated) {
+    console.log('is authenticated')
     return next();
   } else {
+    console.log('is not authenticated')
     return res.status(401).redirect('/login');
   }
 
