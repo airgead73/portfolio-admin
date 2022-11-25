78bcd5a88dd8e9a57aa380b27a4bfdbbc11493a1
Dan Moneypenny <catullus1973@gmail.com>
2022-11-25 04:03:29 -0800

    update

--- app/units.api/router.js
+++ app/units.api/router.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const apiRouter = Router();
 const { workRouter } = require('./works');
+const { photoRouter } = require('./photos');
 
 apiRouter.use('/works', workRouter);
+apiRouter.use('/photos', photoRouter);
 
 module.exports = apiRouter;
