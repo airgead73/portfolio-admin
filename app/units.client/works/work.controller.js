0d142a07fe002834c8c027f182a9f5f63f8f2898
Dan Moneypenny <catullus1973@gmail.com>
2022-11-25 09:48:35 -0800

    add client view unit

---
+++ app/units.client/works/work.controller.js
@@ -0,0 +1,111 @@
+/**
+ * @desc work create view
+ * @route GET - /works/add
+ * @access Private
+ * */
+ exports.create = async(req,res,next) => {
+
+  try {
+
+    res.status(200).send('CLIENT work create view.');
+
+  } catch(err) {
+
+    next(err);
+
+  }
+
+ } 
+/**
+ * @desc work read view
+ * @route GET - /works
+ * @access Private
+ * */
+ exports.read = async(req,res,next) => {
+
+  try {
+
+    res.status(200).send('CLIENT work read view.');
+
+  } catch(err) {
+
+    next(err);
+
+  }
+
+ } 
+/**
+ * @desc work detail view
+ * @route GET - /works/:id
+ * @access Private
+ * */
+ exports.detail = async(req,res,next) => {
+
+  try {
+
+    const { id } = req.params;
+    res.status(200).send(`CLIENT work detail view: ${id}.`);
+
+  } catch(err) {
+
+    next(err);
+
+  }
+
+ } 
+/**
+ * @desc work update view
+ * @route GET - /works/:id/update
+ * @access Private
+ * */
+ exports.update = async(req,res,next) => {
+
+  try {
+
+    const { id } = req.params;
+    res.status(200).send(`CLIENT work update view: ${id}.`);
+
+  } catch(err) {
+
+    next(err);
+
+  }
+
+ } 
+/**
+ * @desc work delete view
+ * @route GET - /works/:id/delete
+ * @access Private
+ * */
+ exports.remove = async(req,res,next) => {
+
+  try {
+
+    const { id } = req.params;
+    res.status(200).send(`CLIENT work delete view: ${id}.`);
+
+  } catch(err) {
+
+    next(err);
+
+  }
+
+ } 
+/**
+ * @desc work drop view
+ * @route GET - /works/drop
+ * @access Private
+ * */
+ exports.drop = async(req,res,next) => {
+
+  try {
+
+    res.status(200).send(`CLIENT work detail view: ${id}.`);
+
+  } catch(err) {
+
+    next(err);
+
+  }
+
+ } 
---
+++ app/units.client/works/work.routes.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const workRouter = Router();
+
+// controllers
+const { create, read, detail, update, remove, drop } = require('./work.controller');
+
+// routes
+workRouter.get('/', read);
+
+authorRouter.get('/add', create);
+
+workRouter.get('/drop', drop);
+
+workRouter.get('/:id', detail);
+
+workRouter.get('/:id/update', update);
+
+workRouter.get('/:id/delete', remove);
+
+module.exports = workRouter;
